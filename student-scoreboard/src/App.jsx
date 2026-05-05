63442ec28d624e35b5fca5da31697b3eb0458ce6
Rishica <your-email@example.com>
2026-05-05 20:09:22 +0530

    Initial commit

---
+++ student-scoreboard/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import StudentTable from "./components/StudentTable";
+import AddStudentForm from "./components/AddStudentForm";
+import "./App.css";
+
+function App() {
+  const [students, setStudents] = useState([
+    { id: 1, name: "Rishica", score: 75 },
+    { id: 2, name: "Aman", score: 35 },
+  ]);
+
+  // Update score
+  const updateScore = (id, newScore) => {
+    const updated = students.map((student) =>
+      student.id === id ? { ...student, score: newScore } : student
+    );
+    setStudents(updated);
+  };
+
+  // Add student
+  const addStudent = (name, score) => {
+    const newStudent = {
+      id: Date.now(),
+      name,
+      score: Number(score),
+    };
+    setStudents([...students, newStudent]);
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      <AddStudentForm addStudent={addStudent} />
+      <StudentTable students={students} updateScore={updateScore} />
+    </div>
+  );
+}
+
+export default App;
